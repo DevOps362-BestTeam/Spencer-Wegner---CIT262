ab2214d8ca431762a2ca08ca54f0f3ffdb1a1fd9
Spencer Wegner <swegner@byui.edu>
2023-01-19 10:43:43 -0700

    1-19(apiTestGoodandBad

---
+++ tests/loginApiBad.test.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+it ("Should get a login", async ()=>{ // async allows us to read from the database by moving on and coming back.
+
+    const loginResponse = await fetch('https://dev.stedi.me/login', { // await fetch allows it to move on while getting the data
+    
+        method: 'POST', // method: POST gives information to the server
+        headers:{
+            'Content-Type': 'application/text'
+        },
+
+        body:JSON.stringify({ // makes the following into JSON text to be read by the server
+            "userName": "spencer@byui.edu",  
+            "password": "adfadsfafdsfa"
+        })
+    })
+    expect(loginResponse.status).toBe(401); // 401 is the value when unauthorized.
+})
